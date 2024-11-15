679ff9f35c2014042d087cf944e629cffb3d2ee2
Kazuki Yoda <yd.tokyu9006f@gmail.com>
2024-11-15 00:45:24 -0800

    Add `install.sh` for CUDA11.8 + Python3.10 + PyTorch2.3.0

---
+++ install.sh
@@ -0,0 +1,19 @@
+python3.10 -m venv venv
+
+source venv/bin/activate
+
+# Make sure Python version is 3.10
+# If using different Python version,
+# change the stable-fast prebuilt wheel accordingly
+python -V
+
+# Make sure CUDA version
+nvcc --version
+
+# This install is for Python3.10 + CUDA 11.8 + PyTorch 2.3.0
+# Change them for different versions of CUDA/PyTorch/Python
+python -m pip install --index-url https://download.pytorch.org/whl/cu118 \
+  torch==2.3.0 xformers>=0.0.22 triton>=2.1.0 \
+  https://github.com/chengzeyi/stable-fast/releases/download/v1.0.5/stable_fast-1.0.5+torch230cu118-cp310-cp310-manylinux2014_x86_64.whl
+
+python -m pip install diffusers>=0.19.3 transformers accelerate
